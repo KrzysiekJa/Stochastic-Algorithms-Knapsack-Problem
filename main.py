5155b76d2eab11f2c6b9c5635756bb3e39136c9b
Kethash <61096082+Kethash@users.noreply.github.com>
2022-12-13 19:37:29 +0100

    Change plotting
    Line -> Bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from genetic import solve_knapsack, print_generation, Individual
 
 if __name__ == '__main__':
@@ -18,10 +19,15 @@ if __name__ == '__main__':
             weights.append(solution.weight())
             solutions.append(solution)
         
-    x = [x for x in range(len(solutions))]
-    print_generation(solutions)
-    plt.figure()
-    plt.plot(x,y)
-    plt.plot(x, weights)
+    x = np.arange(len(solutions))
+    #print_generation(solutions)
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - 0.35/2,y, 0.35, label="Value")
+    rects2 = ax.bar(x + 0.35/2,weights, 0.35, label="Weight")
+    ax.legend()
+    ax.set_xticks(x, [x for x in range(1,len(solutions)+1)])
+    ax.bar_label(rects1, padding=3)
+    ax.bar_label(rects2, padding=3)
+    fig.tight_layout()
     plt.show()
     
