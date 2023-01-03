fa4b7ad3ce0f97e73d484f972b9de3cc075ed98f
KrzysiekJa <kjmj77@gmail.com>
2023-01-04 00:25:49 +0100

    Fixed small bugs connected with initialization of velocities.

--- Dynamic_Knapsack/PSOKnapsak.py
+++ Dynamic_Knapsack/PSOKnapsak.py
@@ -4,15 +4,6 @@ import numpy as np
 from decorator import bench
 
 
-W = 5
-wt = [1,2,10,200,3]
-val = [2,5,6,1,100]
-
-# W = maximum capacity
-# wt = weight
-# val = value
-# n = size of the objects
-# s = size of particles
 
 #introduce a flexible penality, exponential scaling, take the exponetial value instead the normal fitness it should grow faster
 def fitness_function( x, w, v, W ):
@@ -24,8 +15,14 @@ def fitness_function( x, w, v, W ):
 def sigmoid( x ):
     return 1/(1 + np.exp(-x))
 
-
-def solve_pso_knapsack( W, wt, vals, n, n_particles, epochs = 5, C1 = 2.0, C2 = 2.0):
+'''
+    W = maximum capacity
+    wt = weight
+    val = value
+    n = number of the objects
+    n_particles = number of particles
+'''
+def solve_pso_knapsack( W, wt, val, n, n_particles, epochs = 5, C1 = 2.0, C2 = 2.0):
     particle_params = ['POSITION', 'P_LOCAL_BEST', 'VELOCITY']
     GLOBAL_BEST_VAL = -1
     GLOBAL_BEST = np.zeros(n, dtype=np.int8)
@@ -36,14 +33,14 @@ def solve_pso_knapsack( W, wt, vals, n, n_particles, epochs = 5, C1 = 2.0, C2 = 
     # find the global solution inside the swarm
     for _ in range(n_particles):
         position = np.random.randint( 2, dtype=np.int8, size=n ) # random binary array
-        tmp_val = fitness_function( position, wt, vals, W )
         
+        tmp_val = fitness_function( position, wt, val, W )
         if( tmp_val > GLOBAL_BEST_VAL ):
             GLOBAL_BEST_VAL = tmp_val
             np.copyto( GLOBAL_BEST, position ) # syntax: np.copyto(dst, src)
         
         # particles has position, best_pos, velocity
-        swarm.append( dict( zip(particle_params, [position, position, np.zeros(n)]) ) )
+        swarm.append( dict( zip(particle_params, [position, position, np.random.rand(n)]) ) )
     
     print( ">> ", GLOBAL_BEST_VAL )
     print( "## ", GLOBAL_BEST )
@@ -52,17 +49,16 @@ def solve_pso_knapsack( W, wt, vals, n, n_particles, epochs = 5, C1 = 2.0, C2 = 
         for i, particle in enumerate(swarm):
             
             # checking P_LOCAL_BEST
-            if fitness_function( particle['POSITION'], wt, vals, W ) > fitness_function( particle['P_LOCAL_BEST'], wt, vals, W ):
+            if fitness_function( particle['POSITION'], wt, val, W ) > fitness_function( particle['P_LOCAL_BEST'], wt, val, W ):
                 np.copyto( particle['P_LOCAL_BEST'], particle['POSITION'] )
             
             # checking GLOBAL_BEST
-            if fitness_function( particle['P_LOCAL_BEST'], wt, vals, W ) > GLOBAL_BEST_VAL:
+            if fitness_function( particle['P_LOCAL_BEST'], wt, val, W ) > GLOBAL_BEST_VAL:
                 np.copyto( GLOBAL_BEST, particle['P_LOCAL_BEST'] )
-                GLOBAL_BEST_VAL = fitness_function( GLOBAL_BEST, wt, vals, W ) # update
+                GLOBAL_BEST_VAL = fitness_function( GLOBAL_BEST, wt, val, W ) # update
             
             # random() generates values from range of <0,1>
             particle['VELOCITY'] += C1 * random() * (particle['P_LOCAL_BEST']-particle['POSITION']) + C2 * random() * (GLOBAL_BEST-particle['POSITION'])
-            print(i, '*', particle['VELOCITY'])
             
             ro = np.random.randint( 2, dtype=np.int8, size=n ) # random binary array
             
@@ -83,7 +79,10 @@ def solve_pso_knapsack( W, wt, vals, n, n_particles, epochs = 5, C1 = 2.0, C2 = 
             print( "## ", GLOBAL_BEST )
 
 
-solve_pso_knapsack( W, wt, val, 5, 3 )
+W = 6
+wt = [1,2,10,200,3]
+val = [2,5,6,1,100]
+solve_pso_knapsack( W, wt, val, 5, 4, 3 )
 solve_pso_knapsack( 10, [1,20,10,2,2], [5,100,10,100,1], 5, 2 )
 
 
