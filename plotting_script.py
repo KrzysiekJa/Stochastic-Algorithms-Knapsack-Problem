f8434e56fbf1088d5349b35508796e301beb3de6
KrzysiekJa <kjmj77@gmail.com>
2023-01-18 21:54:40 +0100

    Added plotting_script

--- plotting_script.py
+++ plotting_script.py
@@ -9,7 +9,7 @@ value_dp = [51, 5384, 466567, 49571635]
 time_m  = [0.01980683599999999, 0.2048368511799999, 0.8569423148599995, 5.125439968259998]
 value_m = [50.94, 5289.76, 367048.88, 29500138.44]
 time_G  = [0.006937316700000045, 0.008664259319999932, 0.008115169899999941, 0.018264451079958235]
-value_G = [51.0, 0, 292346.4, 26426515.08]
+value_G = [51.0, 5000.0, 292346.4, 26426515.08]
 
 
 
@@ -21,7 +21,7 @@ ax1.set_title('Time performance')
 ax1.plot(size, time_numpy2, label="dynamic")
 ax1.plot(size, time_m, label="PSO")
 ax1.plot(size, time_G, label="genetic")
-ax2.set_xscale('log')
+ax2.set_yscale('log')
 ax2.set(xlabel='problem size')
 ax2.set_title('Output values')
 ax2.plot(size, value_dp, label="dynamic")
